08557395252ed15cdbb0437ef0eeda178d9b3599
Dmitri Babaev <dmitri.babaev@gmail.com>
2017-04-27 16:59:32 +0300

    days to delta transformer, target share transformer parallelization

--- ml_tools/transformers.py
+++ ml_tools/transformers.py
@@ -55,6 +55,14 @@ class CountEncoder(BaseEstimator, TransformerMixin):
         return res
 
 
+def build_categorical_feature_encoder(df, y, col, target_label):
+    vc = df[col].value_counts(dropna=False)
+    true_vc = df[y == target_label][col].value_counts(dropna=False)
+    entries = (true_vc / vc).sort_values(ascending=False).index
+    encoder = dict(zip(entries, range(len(entries))))
+    return col, encoder
+
+
 class TargetShareCountEncoder(BaseEstimator, TransformerMixin):
     """
     >>> import pandas as pd
@@ -64,16 +72,24 @@ class TargetShareCountEncoder(BaseEstimator, TransformerMixin):
     [2, 0, 0, 2, 2, 1, 1]
     """
 
-    def __init__(self, target_label=1):
+    def __init__(self, target_label=1, columns=None, n_jobs=1):
         self.vc = dict()
         self.target_label = target_label
+        self.columns = columns
+        self.n_jobs = n_jobs
 
     def fit(self, df, y):
-        for col in df.select_dtypes(include=['object']):
-            vc = df[col].value_counts(dropna=False)
-            true_vc = df[y == self.target_label][col].value_counts(dropna=False)
-            entries = (true_vc / vc).sort_values(ascending=False).index
-            self.vc[col] = dict(zip(entries, range(len(entries))))
+        from sklearn.externals.joblib import Parallel, delayed
+
+        if self.columns is None:
+            columns = df.select_dtypes(include=['object'])
+        else:
+            columns = self.columns
+
+        self.vc = dict(Parallel(n_jobs=self.n_jobs)(
+            delayed(build_categorical_feature_encoder)(df, y, col, self.target_label)
+            for col in columns
+        ))
 
         return self
 
@@ -97,3 +113,28 @@ def field_list(field_names):
     from functools import partial
     f = partial(field_list_func, field_names=field_names)
     return FunctionTransformer(func=f, validate=False)
+
+
+def days_to_delta_func(df, column_names, base_column):
+    import numpy as np
+    import pandas as pd
+    delta = np.timedelta64(1, 'D')
+    base_col_date = pd.to_datetime(df[base_column], errors='coerce')
+    for col in column_names:
+        days_open = ((base_col_date - pd.to_datetime(df[col], errors='coerce')) / delta).astype(np.int16)
+        df[col] = days_open
+    return df
+
+
+def days_to_delta(column_names, base_column):
+    """
+    >>> import pandas as pd
+    >>> df = pd.DataFrame({'A': ['2015-01-02', '2016-03-20'], 'B': ['2016-02-02', '2016-10-22']})
+    >>> days_to_delta(['A'], 'B').fit_transform(df).A.tolist()
+    [396, 216]
+    """
+    from sklearn.preprocessing import FunctionTransformer
+    from functools import partial
+    f = partial(days_to_delta_func, column_names=column_names, base_column=base_column)
+    d2d = FunctionTransformer(func=f, validate=False)
+    return d2d
