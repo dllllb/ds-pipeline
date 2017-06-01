22351899ad4a3c4596293b1c62685bf2840fc211
Dmitri Babaev <dmitri.babaev@gmail.com>
2017-06-01 16:58:13 +0300

    code in pickle fix

--- spark_tools/core.py
+++ spark_tools/core.py
@@ -64,7 +64,7 @@ def score(sc, sdf, model_path, cols_to_save, target_class=1, code_in_pickle=Fals
             existing_cols_to_save = list(set(cols_to_save).intersection(features_df.columns))
             res_df = features_df[existing_cols_to_save].copy()
 
-            res_df['target_proba'] = mdl.value.predict_proba(features_df)[:, target_class]
+            res_df['target_proba'] = mdl.predict_proba(features_df)[:, target_class]
 
             for e in json.loads(res_df.to_json(orient='records')):
                 yield e
