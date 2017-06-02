1d901775e472be812e2f348b0fb096b700332889
Valyayev Ilya Aleksandrovich <iavalya1@0001mediahdp01.msk.mts.ru>
2017-06-02 18:24:29 +0300

    unicode2str

--- spark_tools/core.py
+++ spark_tools/core.py
@@ -179,14 +179,14 @@ def write(conf, sdf):
     elif storage == 'single-csv':
         data_path = conf['query']
         header = conf.get_bool('header', True)
-        sep = conf.get('sep', '\t')
-        decimal = conf.get('decimal', '.')
+        sep = str(conf.get('sep', '\t'))
+        decimal = str(conf.get('decimal', '.'))
         pdf = sdf.toPandas()
         pdf.to_csv(data_path, sep=sep, header=header, decimal=decimal, encoding='utf8', index=False)
     elif storage == 'csv':
         data_path = conf['query']
         header = conf.get_bool('header', True)
-        sep = conf.get('sep', '\t')
+        sep = str(conf.get('sep', '\t'))
 
         save_to_csv(sdf, data_path, header, sep)
     else:
