954473b2c9b2661031cd6a581c5075a576752886
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-03-14 01:09:34 +0300

    pytest

--- dstools/ml/ensemble_tests.py
+++ dstools/ml/ensemble_tests.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import unittest
 import pandas as pd
 
 from sklearn.model_selection import cross_val_score
@@ -23,173 +22,160 @@ def roc_auc_avg_score(y_true, y_score):
     return roc_auc_score(y_bin, y_score)
 
 
-class TestModelEnsemble(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
+def test_model_ensemble():
+    iris = load_iris()
 
-        est = ModelEnsemble(
-            assembly_estimator=RandomForestClassifier(n_estimators=50, random_state=1),
-            intermediate_estimators=[
-                LogisticRegression(random_state=1),
-                GaussianNB()
-            ]
-        )
+    est = ModelEnsemble(
+        assembly_estimator=RandomForestClassifier(n_estimators=50, random_state=1),
+        intermediate_estimators=[
+            LogisticRegression(random_state=1),
+            GaussianNB()
+        ]
+    )
 
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
 
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
 
-        print(scores.mean(), scores.std())
-
-
-class TestModelEnsembleRegressor(unittest.TestCase):
-    def test_simple(self):
-        boston = load_boston()
-        est = ModelEnsembleRegressor(
-            intermediate_estimators=[
-                LinearRegression(),
-                RandomForestRegressor(n_estimators=5)
-            ],
-            assembly_estimator=LinearRegression()
-        )
-
-        scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
-
-        print(scores.mean(), scores.std())
+    print(scores.mean(), scores.std())
 
 
-class TestModelEnsembleMean(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
-        est = ModelEnsembleMean(
-            intermediate_estimators=[
-                LogisticRegression(random_state=1),
-                GaussianNB()
-            ]
-        )
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+def test_model_ensemble_regressor():
+    boston = load_boston()
 
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+    est = ModelEnsembleRegressor(
+        intermediate_estimators=[
+            LinearRegression(),
+            RandomForestRegressor(n_estimators=5)
+        ],
+        assembly_estimator=LinearRegression()
+    )
 
-        print(scores.mean(), scores.std())
+    scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
 
-
-class TestOneVsRestEnsemble(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
-        est = OneVsRestEnsemble(
-            intermediate_estimators=[
-                LogisticRegression(),
-                GaussianNB(),
-                RandomForestClassifier(n_estimators=5)
-            ],
-            assembly_estimator=RandomForestClassifier(n_estimators=10)
-        )
-
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
-
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
-
-        print(scores.mean(), scores.std())
+    print(scores.mean(), scores.std())
 
 
-class TestModelEnsembleMeanRegressor(unittest.TestCase):
-    def test_simple(self):
-        boston = load_boston()
-        est = ModelEnsembleMeanRegressor(
-            intermediate_estimators=[
-                LinearRegression(),
-                RandomForestRegressor(n_estimators=5)
-            ]
-        )
+def test_model_ensemble_mean():
+    iris = load_iris()
+    est = ModelEnsembleMean(
+        intermediate_estimators=[
+            LogisticRegression(random_state=1),
+            GaussianNB()
+        ]
+    )
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
 
-        scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
 
-        print(scores.mean(), scores.std())
-
-
-class TestKFoldStackingFullRegressor(unittest.TestCase):
-    def test_simple(self):
-        boston = load_boston()
-        est = KFoldStackingFullRegressor(
-            final_estimator=RandomForestRegressor(n_estimators=50, random_state=1),
-            intermediate_estimators=[
-                Ridge(random_state=1),
-                DecisionTreeRegressor()
-            ],
-            n_folds=2
-        )
-
-        scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
-
-        print(scores.mean(), scores.std())
+    print(scores.mean(), scores.std())
 
 
-class TestKFoldStackingFull(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
-        est = KFoldStackingFull(
-            final_estimator=LogisticRegression(),
-            intermediate_estimators=[
-                LogisticRegression(random_state=1),
-                GaussianNB()
-            ],
-            n_folds=2
-        )
+def test_one_vs_rest_ensemble():
+    iris = load_iris()
+    est = OneVsRestEnsemble(
+        intermediate_estimators=[
+            LogisticRegression(),
+            GaussianNB(),
+            RandomForestClassifier(n_estimators=5)
+        ],
+        assembly_estimator=RandomForestClassifier(n_estimators=10)
+    )
 
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
 
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
 
-        print(scores.mean(), scores.std())
-
-
-class TestKFoldStacking(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
-        est = KFoldStacking(
-            final_estimator=RandomForestClassifier(n_estimators=50, random_state=1),
-            intermediate_estimators=[
-                LogisticRegression(random_state=1),
-                GaussianNB()
-            ],
-            n_folds=2
-        )
-
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
-
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
-
-        print(scores.mean(), scores.std())
+    print(scores.mean(), scores.std())
 
 
-class TestForcedMultilabelModel(unittest.TestCase):
-    def test_simple(self):
-        iris = load_iris()
-        est = ForcedMultilabelModel(LogisticRegression())
+def test_model_ensemble_mean_regressor():
+    boston = load_boston()
+    est = ModelEnsembleMeanRegressor(
+        intermediate_estimators=[
+            LinearRegression(),
+            RandomForestRegressor(n_estimators=5)
+        ]
+    )
 
-        scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+    scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
 
-        scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
-
-        print(scores.mean(), scores.std())
-
-
-class TestPerGroupRegressor(unittest.TestCase):
-    def test_simple(self):
-        boston = load_boston()
-        features = pd.DataFrame(boston.data, columns=boston.feature_names)
-        features['group'] = (features.AGE % 3).astype(int)
-        labels = pd.Series(boston.target)
-        est = PerGroupRegressor(
-            estimator=LinearRegression(),
-            split_condition='group',
-        )
-
-        scores = cross_val_score(estimator=est, X=features, y=labels, cv=3)
-
-        print(scores.mean(), scores.std())
+    print(scores.mean(), scores.std())
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_kfold_stacking_full_regressor(self):
+    boston = load_boston()
+    est = KFoldStackingFullRegressor(
+        final_estimator=RandomForestRegressor(n_estimators=50, random_state=1),
+        intermediate_estimators=[
+            Ridge(random_state=1),
+            DecisionTreeRegressor()
+        ],
+        n_folds=2
+    )
+
+    scores = cross_val_score(estimator=est, X=boston.data, y=boston.target, cv=3)
+
+    print(scores.mean(), scores.std())
+
+
+def test_kfold_stacking_full():
+    iris = load_iris()
+    est = KFoldStackingFull(
+        final_estimator=LogisticRegression(),
+        intermediate_estimators=[
+            LogisticRegression(random_state=1),
+            GaussianNB()
+        ],
+        n_folds=2
+    )
+
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+
+    print(scores.mean(), scores.std())
+
+
+def test_kfold_stacking():
+    iris = load_iris()
+    est = KFoldStacking(
+        final_estimator=RandomForestClassifier(n_estimators=50, random_state=1),
+        intermediate_estimators=[
+            LogisticRegression(random_state=1),
+            GaussianNB()
+        ],
+        n_folds=2
+    )
+
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+
+    print(scores.mean(), scores.std())
+
+
+def test_forced_multilabel_model():
+    iris = load_iris()
+    est = ForcedMultilabelModel(LogisticRegression())
+
+    scorer = make_scorer(roc_auc_avg_score, needs_proba=True)
+
+    scores = cross_val_score(estimator=est, X=iris.data, y=iris.target, cv=3, scoring=scorer)
+
+    print(scores.mean(), scores.std())
+
+
+def test_per_group_regressor(self):
+    boston = load_boston()
+    features = pd.DataFrame(boston.data, columns=boston.feature_names)
+    features['group'] = (features.AGE % 3).astype(int)
+    labels = pd.Series(boston.target)
+    est = PerGroupRegressor(
+        estimator=LinearRegression(),
+        split_condition='group',
+    )
+
+    scores = cross_val_score(estimator=est, X=features, y=labels, cv=3)
+
+    print(scores.mean(), scores.std())
