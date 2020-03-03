6878a9a4018d2a38d06d976b9d952bf8f1a66b60
Dmitri Babaev <dmitri.babaev@gmail.com>
2020-03-03 17:02:17 +0300

    setup fixes

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name='Data Science Tools',
+    name=''ds-tools',
     version='0.3.1',
     description='Data Science Tools for Spark and scikit-learn',
     author='Dmitri Babaev',
     author_email='dmitri.babaev@gmail.com',
-    packages=['ds-tools'],
-    requires=['pandas>=0.20', 'numpy>=1.14', 'scikit-learn>=0.19', ],
+    install_requires=['pandas>=0.20', 'numpy>=1.14', 'scikit-learn>=0.19', ],
 )
